e6d4dbdee5fed2a46a2ab6e313c473248e4f9b35
Sandy <developer.pooch@gmail.com>
2017-11-17 22:25:19 +0530

    Added Unity's Tilemap and few other Unity made scripts

--- Assets/Scripts/Utilities/FPSDisplay.cs
+++ Assets/Scripts/Utilities/FPSDisplay.cs
@@ -4,23 +4,24 @@ using TMPro;
 
 public class FPSDisplay : MonoBehaviour
 {
-	public bool enableFPS = true;
-	public TextMeshProUGUI fpsText;
-	float deltaTime = 0.0f;
-	float msec;
-	float fps;
-	string text;
+    public bool enableFPS = true;
+    public TextMeshProUGUI fpsText;
+    float deltaTime = 0.0f;
+    float msec;
+    float fps;
+    string text;
 
- 
-	void LateUpdate ()
-	{
-		if (enableFPS) {
-			deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-			//msec = deltaTime * 1000.0f;
-			fps = 1.0f / deltaTime;
-			//text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
-			//fpsText.text = text;
-			fpsText.text = fps.ToString ("F0");
-		}
-	}
+
+    void LateUpdate()
+    {
+        if (enableFPS && fpsText != null)
+        {
+            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+            //msec = deltaTime * 1000.0f;
+            fps = 1.0f / deltaTime;
+            //text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            //fpsText.text = text;
+            fpsText.text = fps.ToString("F0");
+        }
+    }
 }
