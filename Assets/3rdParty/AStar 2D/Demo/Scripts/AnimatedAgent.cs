616eb2ec663db829c79d80892909fb378f062993
Sandy <sandeep.mamdikar@gmail.com>
2018-04-09 21:09:02 +0530

    Added my own path finding system.

--- Assets/3rdParty/AStar 2D/Demo/Scripts/AnimatedAgent.cs
+++ Assets/3rdParty/AStar 2D/Demo/Scripts/AnimatedAgent.cs
@@ -4,104 +4,104 @@ using System.Collections;
 
 namespace AStar_2D.Demo
 {
-	/// <summary>
-	/// Example class that shows how the agent class can be inherited from and expanded.
-	/// </summary>
-	public class AnimatedAgent : Agent
-	{
-		// Private
-		private Animator anim = null;
-		private float timer = 0;
-		private bool canBob = true;
+    /// <summary>
+    /// Example class that shows how the agent class can be inherited from and expanded.
+    /// </summary>
+    public class AnimatedAgent : Agent
+    {
+        // Private
+        private Animator anim = null;
+        private float timer = 0;
+        private bool canBob = true;
 
-		// Public
-		/// <summary>
-		/// Should the agent bob up and down when walking.
-		/// </summary>
-		public bool bob = true;
-		/// <summary>
-		/// The speed that the agent will bob up and down.
-		/// </summary>
-		public float bobSpeed = 35f;
-		/// <summary>
-		/// How far the agent will bob up and down.
-		/// </summary>
-		public float bobAmount = 1.4f;
+        // Public
+        /// <summary>
+        /// Should the agent bob up and down when walking.
+        /// </summary>
+        public bool bob = true;
+        /// <summary>
+        /// The speed that the agent will bob up and down.
+        /// </summary>
+        public float bobSpeed = 35f;
+        /// <summary>
+        /// How far the agent will bob up and down.
+        /// </summary>
+        public float bobAmount = 1.4f;
 
-		// Methods
-		/// <summary>
-		/// Called by unity.
-		/// Note that the base method is called. This is essential to initialize the base class.
-		/// </summary>
-		public override void Start ()
-		{
-			// Make sure we call start on the agent class
-			base.Start ();
+        // Methods
+        /// <summary>
+        /// Called by unity.
+        /// Note that the base method is called. This is essential to initialize the base class.
+        /// </summary>
+        public override void Start()
+        {
+            // Make sure we call start on the agent class
+            base.Start();
 
-			// Find the animator controller
-			anim = GetComponent<Animator> ();
-		}
+            // Find the animator controller
+            anim = GetComponent<Animator>();
+        }
 
-		/// <summary>
-		/// Called by Unity.
-		/// Note that the base method is called. This is essential to update the base class.
-		/// </summary>
-		public override void Update ()
-		{
-			// Make sure we update our agents movement
-			base.Update ();
-			// Update the sprite animation so the character is facing the correct direction
-			// Make our agent bob up and down as he walks
-			updateHeadBob ();
-			updateAnimation ();
-		}
+        /// <summary>
+        /// Called by Unity.
+        /// Note that the base method is called. This is essential to update the base class.
+        /// </summary>
+        public override void Update()
+        {
+            // Make sure we update our agents movement
+            base.Update();
+            // Update the sprite animation so the character is facing the correct direction
+            // Make our agent bob up and down as he walks
+            updateHeadBob();
+            updateAnimation();
+        }
 
-		/// <summary>
-		/// Called when the agent is unable to reach a target destination.
-		/// </summary>
-		public override void onDestinationUnreachable ()
-		{
-			print ("I can't reach that target");
-			Debug.LogWarning (string.Format ("Agent [{0}]: I can't reach that target", gameObject.name));
-		}
+        /// <summary>
+        /// Called when the agent is unable to reach a target destination.
+        /// </summary>
+        public override void onDestinationUnreachable()
+        {
+            print("I can't reach that target");
+            Debug.LogWarning(string.Format("Agent [{0}]: I can't reach that target", gameObject.name));
+        }
 
-		private void updateAnimation ()
-		{
-			anim.SetBool ("isMoving", IsMoving);
+        private void updateAnimation()
+        {
+            anim.SetBool("isMoving", IsMoving);
 
-			anim.SetFloat ("Player_Forward", animDirection.y);
-			anim.SetFloat ("Player_Left", animDirection.x);
+            anim.SetFloat("Player_Forward", animDirection.y);
+            anim.SetFloat("Player_Left", animDirection.x);
 
-			//anim.SetFloat ("Player_Forward", animDirection1.y);
-			//	anim.SetFloat ("Player_Left", animDirection1.x);	
-		
-			//debugText.text = animDirection.ToString ("F2");
-		}
+            //anim.SetFloat ("Player_Forward", animDirection1.y);
+            //	anim.SetFloat ("Player_Left", animDirection1.x);	
 
-		private void updateHeadBob ()
-		{
-			// Only allow character bobbing when moving
-			canBob = state == (AgentState.FollowingPath);
+            //debugText.text = animDirection.ToString ("F2");
+        }
 
-			// Make sure head bob is enabled
-			if (bob == false || canBob == false)
-				return;
+        private void updateHeadBob()
+        {
+            // Only allow character bobbing when moving
+            canBob = state == (AgentState.FollowingPath);
 
-			// Add to timer
-			float wave = Mathf.Sin (timer);
+            // Make sure head bob is enabled
+            if (bob == false || canBob == false)
+                return;
 
-			// Add the bob speed
-			timer += bobSpeed * Time.deltaTime;
+            // Add to timer
+            float wave = Mathf.Sin(timer);
 
-			// Clamp bob
-			if (timer > Mathf.PI * 2)
-				timer = timer - (Mathf.PI * 2);
+            // Add the bob speed
+            timer += bobSpeed * Time.deltaTime;
 
-			// Calcualte the change amount
-			float change = wave * bobAmount * Time.deltaTime;
+            // Clamp bob
+            if (timer > Mathf.PI * 2)
+                timer = timer - (Mathf.PI * 2);
 
-			// Update the position
-			transform.position += new Vector3 (0, change, 0);
-		}
-	}
+            // Calcualte the change amount
+            float change = wave * bobAmount * Time.deltaTime;
+
+            // Update the position
+            transform.position += new Vector3(0, change, 0);
+        }
+    }
 }
